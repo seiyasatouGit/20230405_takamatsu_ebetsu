28a2a5b2f19dfad4a982b390ee4fb89f757fc11a
okajifrancis <kyorotako@live.jp>
2023-04-11 21:15:27 +0900

    css修正

--- themes/lightning-child-sample/page-top.php
+++ themes/lightning-child-sample/page-top.php
@@ -15,59 +15,23 @@
                     <div class="card csize">
                         <input id="tg0<?= $count_card?>" style="display: none;" type="checkbox" />
                         <label id="omote" class="fuda csize omote" for="tg0<?= $count_card?>"><?php the_title(); ?>
-                        <a href="<?php the_permalink() ?>">詳しく見る</a></label>
+                        <a class="more_button" href="<?php the_permalink() ?>">詳しく見る</a></label>
                         <label id=<?= $card_face ?> class="fuda csize" for="tg0<?= $count_card?>"></label>
                     </div>
                     <?php $count_card+=1; ?>
-                    <?php if($card_face == "ura__blue"){
+                    <!-- <?php if($card_face == "ura__blue"){
                         $card_face = "ura__brown";
                     }else{
                         $card_face = "ura__blue";
-                    }; ?>
+                    }; ?> -->
 
                     <?php endwhile; endif; ?>
-                    <!-- <div class="card csize">
-                        <input id="tg02" style="display: none;" type="checkbox" />
-                        <label id="omote" class="fuda csize omote" for="tg02">めくると出てくる文章</label>
-                        <label id="ura__blue" class="fuda csize" for="tg02"></label>
-                    </div>
-                    <div class="card csize">
-                        <input id="tg03" style="display: none;" type="checkbox" />
-                        <label id="omote" class="fuda csize omote" for="tg03">めくると出てくる文章</label>
-                        <label id="ura__blue" class="fuda csize" for="tg03"></label>
-                    </div>
-                    <div class="card csize">
-                        <input id="tg04" style="display: none;" type="checkbox" />
-                        <label id="omote" class="fuda csize omote" for="tg04">めくると出てくる文章</label>
-                        <label id="ura__blue" class="fuda csize" for="tg04"></label>
-                    </div>
-
-                    <div class="card csize">
-                        <input id="tg05" style="display: none;" type="checkbox" />
-                        <label id="omote" class="fuda csize omote" for="tg05">めくると出てくる文章</label>
-                        <label id="ura__brown" class="fuda csize" for="tg05"></label>
-                    </div>
-                    <div class="card csize">
-                        <input id="tg06" style="display: none;" type="checkbox" />
-                        <label id="omote" class="fuda csize omote" for="tg06">めくると出てくる文章</label>
-                        <label id="ura__brown" class="fuda csize" for="tg06"></label>
-                    </div>
-                    <div class="card csize">
-                        <input id="tg07" style="display: none;" type="checkbox" />
-                        <label id="omote" class="fuda csize omote" for="tg07">めくると出てくる文章</label>
-                        <label id="ura__brown" class="fuda csize" for="tg07"></label>
-                    </div>
-                    <div class="card csize">
-                        <input id="tg08" style="display: none;" type="checkbox" />
-                        <label id="omote" class="fuda csize omote" for="tg08">めくると出てくる文章</label>
-                        <label id="ura__brown" class="fuda csize" for="tg08"></label>
-                    </div> -->
-
+                   
 
                 </div>
 
                 <div class="call_to_action">
-                    <a href="http://wp8th-ebetsu-takamatsu.local/top">
+                    <a href="http://wp8th-ebetsu-takamatsu.local/top/">
                         <div class="call_to_action__box">
                             <p class="call_to_action__text">カードをシャッフルする</p>
                         </div>
