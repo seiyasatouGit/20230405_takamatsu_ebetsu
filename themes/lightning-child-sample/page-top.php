d0eb94d5c7a940934f9043290f9606b6ab1b483a
okajifrancis <kyorotako@live.jp>
2023-04-11 20:49:49 +0900

    投稿ページランダム配置

--- themes/lightning-child-sample/page-top.php
+++ themes/lightning-child-sample/page-top.php
@@ -6,13 +6,27 @@
                     <h4 class="section-headline__toptext" >カードをクリックするとネタのお題が出るよ。オンラインで話すときにお役に立ててね。</h2>
                 </div>
                 <div class="card-wrap__top">
-
+                <?php $count_card = 1; ?>
+                <?php $card_face = "ura__blue"; ?>
+                <?php query_posts(array('orderby' => 'rand', 'showposts' => 8));
+                    if (have_posts()) :
+                    while (have_posts()) : the_post(); ?>
+            
                     <div class="card csize">
-                        <input id="tg01" style="display: none;" type="checkbox" />
-                        <label id="omote" class="fuda csize omote" for="tg01">めくると出てくる文章</label>
-                        <label id="ura__blue" class="fuda csize" for="tg01"></label>
+                        <input id="tg0<?= $count_card?>" style="display: none;" type="checkbox" />
+                        <label id="omote" class="fuda csize omote" for="tg0<?= $count_card?>"><?php the_title(); ?>
+                        <a href="<?php the_permalink() ?>">詳しく見る</a></label>
+                        <label id=<?= $card_face ?> class="fuda csize" for="tg0<?= $count_card?>"></label>
                     </div>
-                    <div class="card csize">
+                    <?php $count_card+=1; ?>
+                    <?php if($card_face == "ura__blue"){
+                        $card_face = "ura__brown";
+                    }else{
+                        $card_face = "ura__blue";
+                    }; ?>
+
+                    <?php endwhile; endif; ?>
+                    <!-- <div class="card csize">
                         <input id="tg02" style="display: none;" type="checkbox" />
                         <label id="omote" class="fuda csize omote" for="tg02">めくると出てくる文章</label>
                         <label id="ura__blue" class="fuda csize" for="tg02"></label>
@@ -47,7 +61,7 @@
                         <input id="tg08" style="display: none;" type="checkbox" />
                         <label id="omote" class="fuda csize omote" for="tg08">めくると出てくる文章</label>
                         <label id="ura__brown" class="fuda csize" for="tg08"></label>
-                    </div>
+                    </div> -->
 
 
                 </div>
@@ -62,4 +76,10 @@
             </div>
         </div>
     </section>
+
+
+
+
+
+
 <?php get_footer(); ?>
